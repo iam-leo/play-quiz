e08e2a4021ec18df08fa527971dc5a21283d5771
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-11-27 18:23:33 -0300

    add: general structure, fonts, logo, navbar content

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    fontFamily: {
+      'marhey': ['Marhey', 'sans-serif'],
+      'prompt': ['Prompt', 'sans-serif'], //400, 500 italic
+    },
     extend: {},
   },
   plugins: [],
