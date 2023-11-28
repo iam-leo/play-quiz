7eeb79018a6bacc2af57ef6df74a390364ace468
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-11-28 17:54:16 -0300

    add: service to questions, render question and aswers

--- src/app/components/pregunta/pregunta.component.ts
+++ src/app/components/pregunta/pregunta.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pregunta } from 'src/app/Models/Pregunta';
+import { PreguntaService } from 'src/app/services/pregunta.service';
 
 @Component({
   selector: 'app-pregunta',
   templateUrl: './pregunta.component.html',
   styleUrls: ['./pregunta.component.css']
 })
-export class PreguntaComponent {
+export class PreguntaComponent implements OnInit {
+  listPregunta: Pregunta[] = [];
 
+  constructor(public preguntaService: PreguntaService){ }
+
+  ngOnInit(): void {
+    this.listPregunta = this.preguntaService.getPreguntas();
+    console.log(this.listPregunta)
+  }
+
+  getPregunta(){
+    return this.listPregunta[this.preguntaService.indexPregunta].description;
+  }
 }
--- src/app/services/pregunta.service.ts
+++ src/app/services/pregunta.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Pregunta } from '../Models/Pregunta';
+import { Respuesta } from '../Models/Respuesta';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreguntaService {
+  indexPregunta = 0;
 
+  preguntas: Pregunta[] = [
+    new Pregunta('¿Cuál es la capital de Canadá?', [
+      new Respuesta('Vancouver', 0),
+      new Respuesta('Buenos Aires', 0),
+      new Respuesta('Toronto', 1),
+      new Respuesta('Lima', 0)
+    ]),
+    new Pregunta('¿Cuántos Balones de Oro tiene Messi?', [
+      new Respuesta('5', 0),
+      new Respuesta('6', 0),
+      new Respuesta('7', 0),
+      new Respuesta('8', 1)
+    ]),
+    new Pregunta('¿Cómo se llama el Creador de ChatGPT?', [
+      new Respuesta('Steve Jobs', 0),
+      new Respuesta('Sam Altman', 1),
+      new Respuesta('Satya Nadella', 0),
+      new Respuesta('Lee Jae Yong', 0)
+    ]),
+  ]
   constructor() { }
+
+  getPreguntas(): Pregunta[]{
+    return this.preguntas.slice()
+  }
 }
