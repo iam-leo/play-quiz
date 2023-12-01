6ceed1c7a0c8d186c2a3cd8f6bef404354a2977e
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-11-30 21:29:02 -0300

    fix: general styles

--- src/app/components/pregunta/pregunta.component.ts
+++ src/app/components/pregunta/pregunta.component.ts
@@ -34,17 +34,52 @@ export class PreguntaComponent implements OnInit {
   addClassOption(respuesta: Respuesta): any{
     // Respuesta seleccionada y no confirmada
     if(respuesta === this.preguntaService.selectedOption && !this.preguntaService.confirmedQuestion){
-      return 'bg-yellow-400 border-yellow-500 text-black hover:bg-yellow-400 hover:border-yellow-500'
+      return 'bg-yellow-400 border-yellow-500 hover:bg-yellow-400 hover:border-yellow-500'
     }
 
     // Respuesta correcta y confirmada
     if(respuesta === this.preguntaService.selectedOption && this.preguntaService.confirmedQuestion && this.preguntaService.selectedOption.isCorrect === 1){
-      return 'bg-green-500 border-green-600 text-black hover:bg-green-500 hover:border-green-600'
+      return 'bg-green-500 border-green-600 hover:bg-green-500 hover:border-green-600'
     }
 
     // Respuesta incorrecta y confirmada
     if(respuesta === this.preguntaService.selectedOption && this.preguntaService.confirmedQuestion && this.preguntaService.selectedOption.isCorrect === 0){
-      return 'bg-red-600 border-red-700 text-white hover:text-white hover:bg-red-600 hover:border-red-700'
+      return 'bg-red-600 border-red-700 hover:text-white hover:bg-red-600 hover:border-red-700'
+    }
+  }
+
+  selectedAnswer($event: any){
+    // Obtenemos todos los li y le quitamos el text-white
+    const optionsAnswer = document.querySelectorAll('li');
+    const btnTxt = document.querySelector('button')?.textContent;
+    optionsAnswer.forEach(opt => {
+      opt.classList.add('text-white');
+
+      if(btnTxt !== 'Siguiente' && opt.textContent !== $event.target.textContent){
+        opt.classList.add('text-white');
+      } else if(btnTxt !== 'Siguiente' && btnTxt !== 'Finalizar' && !this.preguntaService.confirmedQuestion){
+        console.log('Agregando texto negro')
+        $event.target.classList.add('text-black');
+      } else{
+        return
+      }
+    })
+
+    // Agregamos clases al Li seleccionado
+    if(btnTxt !== 'Siguiente' && btnTxt !== 'Finalizar'){
+      $event.target.classList.remove('text-white');
+    }
+  }
+
+  confirmAnswer($event: any){
+    const txtBtn = $event.target.textContent
+    const optionsAnswer = document.querySelectorAll('li');
+
+    if(txtBtn === 'Siguiente' || txtBtn === 'Finalizar'){
+      optionsAnswer.forEach(opt => {
+        opt.classList.add('text-white');
+        opt.classList.remove('cursor-pointer', 'hover:bg-yellow-400', 'hover:text-black', 'hover:border-yellow-500', 'hover:border-red-700', 'hover:border-green-600');
+      });
     }
   }
 
